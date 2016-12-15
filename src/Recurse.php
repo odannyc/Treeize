386b7c5674444a438341af181735956474d1ecf8
Danny Carrillo <odannycx@gmail.com>
2016-12-15 11:34:47 -0800

    Adds the callback function in the actual recursion of the tree

--- src/Recurse.php
+++ src/Recurse.php
@@ -23,7 +23,8 @@ class Recurse
             $object->parentKey,
             $object->indexKey,
             $object->parentId,
-            $object->childrenKey
+            $object->childrenKey,
+            $object->callback
         );
     }
 
@@ -35,16 +36,18 @@ class Recurse
      * @param $indexKey
      * @param int $parentId
      * @param string $childrenKey
+     * @param callable $callback
      *
      * @return array $finalTree
      */
-    public static function recurse($tree, $parentKey, $indexKey, $parentId, $childrenKey)
+    public static function recurse($tree, $parentKey, $indexKey, $parentId, $childrenKey, $callback)
     {
         $finalTree = [];
 
         foreach ($tree as $value) {
+            $value = $callback($value);
             if ($value[$parentKey] == $parentId) {
-                $children = self::recurse($tree, $parentKey, $indexKey, $value[$indexKey], $childrenKey);
+                $children = self::recurse($tree, $parentKey, $indexKey, $value[$indexKey], $childrenKey, $callback);
                 if ($children) {
                     $value[$childrenKey] = $children;
                 }
